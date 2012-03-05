4e13cda173081e483553a5e06c5cdab062a1b483
maskl <masklod@gmail.com>
2012-03-05 04:27:16 +0100

    Added first database. WIP.

--- Src/Model/CountrySelector.cs
+++ Src/Model/CountrySelector.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Flags
 {
     public class CountrySelector : ICountrySelector
     {
+        private readonly FlagDataContext _flagsDB;
+
+        public CountrySelector()
+        {
+            // WP7? const string dbConnectionString = "datasource='appdata:/flags.sdf'; mode='read only'";
+            const string dbConnectionString = "Data Source='Media/flags.sdf';" +
+                                              "Mode='read only';" +
+                                              "Temp File Directory='Media/'";
+
+            _flagsDB = new FlagDataContext(dbConnectionString);
+        }
+
         public void GetCountriesByParams(ICollection<Country> list, int color, int shape, int add)
         {
             list.Clear();
+
+            foreach (var flag in (from f in _flagsDB.Flags orderby f.Country select f))
+            {
+                list.Add(new Country { Capital="TEst", Continent = "fdsfas", Name = flag.Country, Tag = "pl"});
+            }
+
         }
 
         public void GetAllCountries(ICollection<Country> list)
---
+++ Src/Model/Flag.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace Flags
+{
+    public class FlagDataContext : DataContext
+    {
+        public FlagDataContext(string connectionString)
+            : base(connectionString)
+        {
+            
+        }
+
+        public Table<Flag> Flags;
+    }
+
+    [Table]
+    public class Flag : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        private string _iso;
+        [Column(IsPrimaryKey = true)]
+        public string Iso
+        {
+            get { return _iso; }
+            set
+            {
+                NotifyPropertyChanging("Iso");
+                _iso = value;
+                NotifyPropertyChanged("Iso");
+            }
+        }
+
+        private string _country;
+        [Column]
+        public string Country
+        {
+            get { return _country; }
+            set
+            {
+                NotifyPropertyChanging("Country");
+                _country = value;
+                NotifyPropertyChanged("Country");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangingEventHandler PropertyChanging;
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+    }
+}
