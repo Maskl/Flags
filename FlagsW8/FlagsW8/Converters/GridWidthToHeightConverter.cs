822dd48f7afc21ab877a780ffebd2ed5e244f73a
Marek Sklodowski <masklod@gmail.com>
2012-07-21 21:49:26 +0200

    Basics of metro UI

---
+++ FlagsW8/FlagsW8/Converters/GridWidthToHeightConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace FlagsW8.Converters
+{
+    public sealed class GridToHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (int)value * 62 / 122;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return (int)value * 122 / 62;
+        }
+    }
+}
